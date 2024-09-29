639f424b1c02973e210f70a66fe4ea8a44d3fbea
hugomufraggi <hugo.mufraggi@epitech.eu>
2024-09-29 17:05:03 +0200

    add shop and gallery

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    assetPrefix: '/blue-legion-onepage/',
-    basePath: '/blue-legion-onepage',
     images: {
         unoptimized: true,
     },
